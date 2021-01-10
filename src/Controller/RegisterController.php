cf7b39bf18bc6167fd6d22d13f725933c608a0a7
Déb <deborrah.brunier.simplon@gmail.com>
2021-01-10 18:32:38 +0100

    add fields form + style

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegisterType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +13,13 @@ class RegisterController extends AbstractController
     /**
      * @Route("/inscription", name="register")
      */
-    public function index(): Response
+    public function index()
     {
-        return $this->render('register/index.html.twig');
+        $user = new User();
+        $form = $this->createForm( RegisterType::class, $user);
+        
+        return $this->render('register/index.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
