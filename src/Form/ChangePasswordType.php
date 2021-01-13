3c445f0501acb25123290f8c184e6143e3011b41
Déb <deborrah.brunier.simplon@gmail.com>
2021-01-13 17:08:47 +0100

    disable change on fields except password

--- src/Form/ChangePasswordType.php
+++ src/Form/ChangePasswordType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +14,16 @@ class ChangePasswordType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class, [
+                'disabled' => true
+            ])
             ->add('password')
-            ->add('firstname')
-            ->add('lastname')
+            ->add('firstname', TextType::class, [
+                'disabled' => true
+            ])
+            ->add('lastname', TextType::class, [
+                'disabled' => true
+            ])
         ;
     }
 
