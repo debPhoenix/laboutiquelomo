231e38e546a82a471eb27f2f00a5cfce12fc1c0e
Déb <deborrah.brunier.simplon@gmail.com>
2021-01-13 18:12:57 +0100

    add new passeword fields + mapped key on

--- src/Form/ChangePasswordType.php
+++ src/Form/ChangePasswordType.php
@@ -5,7 +5,10 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,15 +17,47 @@ class ChangePasswordType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', EmailType::class, [
-                'disabled' => true
-            ])
-            ->add('password')
-            ->add('firstname', TextType::class, [
-                'disabled' => true
-            ])
             ->add('lastname', TextType::class, [
-                'disabled' => true
+                'disabled' => true,
+                'label' => 'Nom'
+            ])
+
+            ->add('firstname', TextType::class, [
+                'disabled' => true,
+                'label' => 'Prénom'
+            ])
+            ->add('email', EmailType::class, [
+                'disabled' => true,
+                'label' => 'Adresse email'
+            ])
+
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe actuel',
+                'attr' => [
+                    'placeholder' => 'Veuillez saisir votre mot de passe actuel'
+                ]
+            ])
+            ->add('new_password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'invalid_message' => 'Les mots de passe doivent être identiques.',
+                'label' => 'Nouveau mot de passe',
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Nouveau mot de passe',
+                    'attr' => [
+                        'placeholder' => 'saisir le nouveau mot de passe'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer le nouveau mot de passe',
+                    'attr' => [
+                        'placeholder' => 'confirmer le nouveau mot de passe'
+                    ]
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => "Mettre à jour"
             ])
         ;
     }
