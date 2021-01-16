642887b0b6db90c79aa3d716e31853af17502b94
Déb <deborrah.brunier.simplon@gmail.com>
2021-01-17 00:30:18 +0100

    findall product entity + for to display products

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/nos-produits", name="products")
      */
-    public function index(): Response
+    public function index()
     {
-        return $this->render('product/index.html.twig');
+        $products = $this->entityManager->getRepository( Product::class)->findAll();
+
+        return $this->render('product/index.html.twig', [
+            'products'=>$products
+        ]);
     }
 }
