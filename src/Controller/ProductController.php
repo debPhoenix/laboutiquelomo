3652f162ffcfad0dbaf8d6fab988e17c3f8b4100
Déb <deborrah.brunier.simplon@gmail.com>
2021-01-19 17:50:26 +0100

    fixe price + product page

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -12,20 +12,33 @@ class ProductController extends AbstractController
 {
     private $entityManager;
 
-    public function __construct(EntityManagerInterface $entityManager)
-    {
+    public function __construct(EntityManagerInterface $entityManager) {
         $this->entityManager = $entityManager;
     }
 
     /**
      * @Route("/nos-produits", name="products")
      */
-    public function index()
-    {
+    public function index() {
         $products = $this->entityManager->getRepository( Product::class)->findAll();
 
         return $this->render('product/index.html.twig', [
             'products'=>$products
+        ]);
+    }
+
+    /**
+     * @Route("/produit/{slug}", name="product")
+     */
+    public function show($slug) {
+        $product = $this->entityManager->getRepository( Product::class)->findOneBySlug($slug);
+
+        if (!$product){
+            return $this->redirectToRoute('products');
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product'=>$product
         ]);
     }
 }
