5e242d6056971e17055a1cb2643bd1dbcaa4e952
Déb <deborrah.brunier.simplon@gmail.com>
2021-01-23 21:13:14 +0100

    add/ decrease quantity + icons + cant add on url false id product

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -21,17 +21,8 @@ class CartController extends AbstractController
      */
     public function index(Cart $cart)
     {
-        $cartComplete = [];
-
-        foreach ($cart->get() as $id => $quantity) {
-            $cartComplete[] =[
-                'product' => $this->entityManager->getRepository(Product::class)->findOneById($id),
-                'quantity' => $quantity
-            ];
-        }
-  
         return $this->render('cart/index.html.twig', [
-            'cart' => $cartComplete
+            'cart' => $cart->getFull()
         ]);
     }
 
@@ -56,12 +47,23 @@ class CartController extends AbstractController
         return $this->redirectToRoute('products');
     }
 
+
     /**
      * @Route("/cart/delete{id}", name="delete_to_cart")
      */
     public function delete(Cart $cart, $id)
     {
         $cart->delete($id);
+
+        return $this->redirectToRoute('cart');
+    }
+
+    /**
+     * @Route("/cart/decrease{id}", name="decrease_to_cart")
+     */
+    public function decrease(Cart $cart, $id)
+    {
+        $cart->decrease($id);
 
         return $this->redirectToRoute('cart');
     }
