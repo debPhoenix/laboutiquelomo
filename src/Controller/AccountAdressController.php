9919e6e6a97696ab01e69e99b3dd88a4eae634b6
Déb <deborrah.brunier.simplon@gmail.com>
2021-01-24 17:55:33 +0100

    view all addresses in account

--- src/Controller/AccountAdressController.php
+++ src/Controller/AccountAdressController.php
@@ -4,13 +4,22 @@ namespace App\Controller;
 
 use App\Entity\Adress;
 use App\Form\AdressType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccountAdressController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
-     * @Route("/compte/adresses", name="account_adress")
+     * @Route("/compte/adresses", name="account_address")
      */
     public function index()
     {
@@ -21,11 +30,20 @@ class AccountAdressController extends AbstractController
     /**
      * @Route("/compte/ajouter-une-adresse", name="account_address_add")
      */
-    public function add()
+    public function add(Request $request)
     {
         $adress = new Adress();
 
         $form= $this->createForm(AdressType::class, $adress);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $adress->setUser($this->getUser());
+            $this->entityManager->persist($adress);
+            $this->entityManager->flush();
+            return $this->redirectToRoute('account_address');
+        }
 
         return $this->render('account/adress_add.html.twig',[
             'form' => $form->createView()
