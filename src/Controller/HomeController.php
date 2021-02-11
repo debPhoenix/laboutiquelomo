30d27d00ea2205386063a0b0dd7c185a88c0b090
Déb <deborrah.brunier.simplon@gmail.com>
2021-02-11 12:29:59 +0100

    add bestsellers to homepage + single product page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/ ", name="home")
      */
     public function index()
     {
-        return $this->render('home/index.html.twig');
+        $products = $this->entityManager->getRepository(Product::class)->findByIsBest(1);
+
+        return $this->render('home/index.html.twig', [
+            'products' => $products
+        ]);
     }
 }
